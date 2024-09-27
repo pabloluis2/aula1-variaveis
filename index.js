03a46fe4a309920754a6ee5b64dcb937a9cd4207
pabloluis2 <pabloluis2@gmail.com>
2024-09-26 21:42:30 -0300

    Aprendendo comandos básicos em javascript

---
+++ index.js
@@ -0,0 +1,12 @@
+// isso é um comentario de uma linha
+/*
+Esse é um comentário multilinha
+*/
+
+//alert("Olha o JS aqui!")
+
+//imprimindo no console do navegador
+
+
+let nome = prompt("Qual o seu nome?")
+console.log("Seu nome é: ",nome)
